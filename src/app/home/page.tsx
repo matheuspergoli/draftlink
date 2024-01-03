f5483c0d249a58d7691ffb9468aba5d3e04f78f1
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2024-01-03 02:14:08 -0300

    feat: add link to the application from home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,13 @@
+import { env } from '@/environment/env'
 import { cn, placeholderBlurhash } from '@/libs/utils'
 import { BlurImage } from '@/shared/components/blur-image'
 import { ThemeMode } from '@/shared/components/theme-mode'
-import { buttonVariants } from '@/shared/ui/button'
+import { Button, buttonVariants } from '@/shared/ui/button'
 
 export default async function Page() {
+	const isHttp = env.VERCEL_URL ? 'https://' : 'http://'
+	const redirectLink = `app.${env.NEXT_PUBLIC_ROOT_DOMAIN}/login`
+
 	return (
 		<>
 			<header className='mb-40 border-b'>
@@ -12,11 +16,11 @@ export default async function Page() {
 					<ul className='flex gap-3'>
 						<li>
 							<a
-								href='https://github.com/matheuspergoli/next-template'
+								href='https://github.com/matheuspergoli/draftlink'
 								target='_blank'
 								rel='noopener noreferrer'
 								className={cn(buttonVariants({ variant: 'outline' }), 'font-semibold')}>
-								Repository
+								Repositório
 							</a>
 						</li>
 						<li>
@@ -38,9 +42,21 @@ export default async function Page() {
 						Next.js 14 Template
 					</h1>
 					<p className='text-gray-500 md:text-xl'>
-						Created by <span className='underline'>Matheus Pergoli</span> for personal
-						use. Feel free to use it
+						Projeto foi feito utilizando o template para Next.js 14 criado por Matheus
+						Pergoli.
 					</p>
+					<p className='text-gray-500 md:text-xl'>
+						Esse template foi criado para o uso pessoal mas é gratuito para qualquer um
+						usar.
+					</p>
+				</section>
+
+				<section>
+					<Button asChild>
+						<a href={`${isHttp}${redirectLink}`} target='_blank' rel='noreferrer'>
+							Ir para o site
+						</a>
+					</Button>
 				</section>
 
 				<figure className='overflow-hidden rounded-lg'>
