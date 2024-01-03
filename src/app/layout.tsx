1d0162de58df5b6d507a4eb7d27c7a3028dddc11
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2024-01-03 15:09:19 -0300

    feat: add metadata to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,35 @@ import type { Metadata, Viewport } from 'next'
 
 import { SessionProvider } from '@/context/session-provider'
 import { ThemeProvider } from '@/context/theme-provider'
+import { env } from '@/environment/env'
 import { Provider } from '@/provider'
 import { Toaster } from '@/shared/ui/sonner'
 
+const metadataBase = env.VERCEL_URL
+	? new URL(`https://${env.NEXTAUTH_URL}`)
+	: new URL(`http://${env.NEXTAUTH_URL}`)
+
 export const metadata: Metadata = {
 	title: 'DraftLink',
-	description: 'Created by Matheus Pergoli'
+	description: 'Site para criação de Linktrees criado por Matheus Pergoli',
+	openGraph: {
+		title: 'DraftLink',
+		description: 'Site para criação de Linktrees criado por Matheus Pergoli',
+		images: [
+			'https://public.blob.vercel-storage.com/eEZHAoPTOBSYGBE3/JRajRyC-PhBHEinQkupt02jqfKacBVHLWJq7Iy.png'
+		],
+		creators: ['DraftLink']
+	},
+	twitter: {
+		card: 'summary_large_image',
+		title: 'DraftLink',
+		description: 'Site para criação de Linktrees criado por Matheus Pergoli',
+		images: [
+			'https://public.blob.vercel-storage.com/eEZHAoPTOBSYGBE3/JRajRyC-PhBHEinQkupt02jqfKacBVHLWJq7Iy.png'
+		],
+		creator: 'by DraftLink'
+	},
+	metadataBase
 }
 
 export const viewport: Viewport = {
